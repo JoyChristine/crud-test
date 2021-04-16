4dc7b858bfd8559de6559f512f1f1ad436052cc5
Joy Christine <joychristin2@gmail.com>
2021-04-16 15:26:37 +0300

    feature/3: Implemented creating an order for a menu item

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -3,26 +3,26 @@ module.exports = app => {
 
     var router = require("express").Router();
 
-    // Create a new Tutorial
-    router.post("/", tutorials.create);
+    // // Create a new Tutorial
+    // router.post("/", tutorials.create);
 
-    // Retrieve all Tutorials
-    router.get("/", tutorials.findAll);
+    // // Retrieve all Tutorials
+    // router.get("/", tutorials.findAll);
 
-    // Retrieve all published Tutorials
-    router.get("/published", tutorials.findAllPublished);
+    // // Retrieve all published Tutorials
+    // router.get("/published", tutorials.findAllPublished);
 
-    // Retrieve a single Tutorial with id
+    // Retrieve a single Tutorial with id feature 3
     router.get("/:id", tutorials.findOne);
 
-    // Update a Tutorial with id
-    router.put("/:id", tutorials.update);
+    // // Update a Tutorial with id
+    // router.put("/:id", tutorials.update);
 
-    // Delete a Tutorial with id
-    router.delete("/:id", tutorials.delete);
+    // // Delete a Tutorial with id
+    // router.delete("/:id", tutorials.delete);
 
-    // Create a new Tutorial
-    router.delete("/", tutorials.deleteAll);
+    // // Create a new Tutorial
+    // router.delete("/", tutorials.deleteAll);
 
     app.use("/api/tutorials", router);
 };
