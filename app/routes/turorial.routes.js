fe1eaa1abab8235028b5297f002e6cfc3a4f4875
Joy Christine <joychristin2@gmail.com>
2021-04-16 15:22:03 +0300

    feature/1: Implemented menu creation

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -3,26 +3,26 @@ module.exports = app => {
 
     var router = require("express").Router();
 
-    // Create a new Tutorial
+    // Create a new Tutorial feature 1
     router.post("/", tutorials.create);
 
-    // Retrieve all Tutorials
-    router.get("/", tutorials.findAll);
+    // // Retrieve all Tutorials feature 2 
+    // router.get("/", tutorials.findAll);
 
-    // Retrieve all published Tutorials
-    router.get("/published", tutorials.findAllPublished);
+    // // Retrieve all published Tutorials
+    // router.get("/published", tutorials.findAllPublished);
 
-    // Retrieve a single Tutorial with id
-    router.get("/:id", tutorials.findOne);
+    // // Retrieve a single Tutorial with id feature 3
+    // router.get("/:id", tutorials.findOne);
 
-    // Update a Tutorial with id
-    router.put("/:id", tutorials.update);
+    // // Update a Tutorial with id
+    // router.put("/:id", tutorials.update);
 
-    // Delete a Tutorial with id
-    router.delete("/:id", tutorials.delete);
+    // // Delete a Tutorial with id
+    // router.delete("/:id", tutorials.delete);
 
-    // Create a new Tutorial
-    router.delete("/", tutorials.deleteAll);
+    // // Create a new Tutorial
+    // router.delete("/", tutorials.deleteAll);
 
     app.use("/api/tutorials", router);
 };
